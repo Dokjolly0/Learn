6988333320cfcb8c294a8a4f5f01769aa30642ee
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-08-24 20:10:32 +0200

    Update user component angular 01-project

--- Angular/project-1/01-starting-project/src/app/user/user.component.ts
+++ Angular/project-1/01-starting-project/src/app/user/user.component.ts
@@ -1,25 +1,21 @@
 import { Component } from '@angular/core';
-import { DUMMY_USERS as userList} from '../dummy-users';
+import { DUMMY_USERS as userList } from '../dummy-users';
 
-const randomIndex = Math.trunc( Math.random() * userList.length )
-
+const randomIndex = Math.floor(Math.random() * 6);
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
   selectedUser = userList[randomIndex];
-
-  //Getter che permette di costruire la path dell'avatar
-  get avatarPath(){
-    return 'assets/users/' + this.selectedUser.avatar
+  get avatarPath() {
+    return `assets/users/${this.selectedUser.avatar}`;
   }
-
-  onSelectUser(){
-    const newRandomIndex = Math.floor(Math.random() * userList.length);
-    this.selectedUser = userList[newRandomIndex];
+  changeUser() {
+    const randomIndex = Math.floor(Math.random() * 6);
+    this.selectedUser = userList[randomIndex];
   }
 }
